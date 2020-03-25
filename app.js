a1affd9ebb668c02d08472a5c469d2334af77474
Alussius Aloy <louierue15@gmail.com>
2020-03-24 20:57:23 -0400

    testing fix number 6

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.get("*", (req, res) => {
     res.send("Error!!");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
     console.log("Server is listening");
 });
